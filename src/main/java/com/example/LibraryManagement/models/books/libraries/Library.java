a2d5af3e8ccb7b2e702382a43110feff6dcfd2e5
TD-Null <tdeligero1209@gmail.com>
2021-12-20 16:08:01 -0500

    Updated comments and properties regarding books.

--- src/main/java/com/example/LibraryManagement/models/books/libraries/Library.java
+++ src/main/java/com/example/LibraryManagement/models/books/libraries/Library.java
@@ -1,6 +1,6 @@
 package com.example.LibraryManagement.models.books.libraries;
 
-import com.example.LibraryManagement.models.books.properties.Book;
+import com.example.LibraryManagement.models.books.properties.BookItem;
 import com.example.LibraryManagement.models.datatypes.Address;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
@@ -10,6 +10,18 @@ import javax.validation.constraints.NotBlank;
 import java.util.HashSet;
 import java.util.Set;
 
+/*
+ * Description:
+ * The central part of the organization for which this software has been designed.
+ * It has attributes like ‘Name’ to distinguish it from any other libraries and
+ * ‘Address’ to describe its location.
+ *
+ * Each library will contain its own books as well as racks to contain the books
+ * for their locations within the library.
+ *
+ * Librarians can also modify libraries to add book items and additional racks
+ * to a given library.
+ */
 @Data
 @Entity
 @Table
@@ -25,10 +37,18 @@ public class Library
     private Address address;
 
     @JsonIgnore
-    @OneToMany(mappedBy = "library", cascade = CascadeType.ALL)
-    private Set<Book> books = new HashSet<>();
+    @OneToMany(mappedBy = "library", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<BookItem> books = new HashSet<>();
 
     @JsonIgnore
-    @OneToMany(mappedBy = "library", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "library", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private Set<Rack> racks = new HashSet<>();
+
+    public void addBookItem(BookItem b) { books.add(b); }
+
+    public void addRack(Rack r) { racks.add(r); }
+
+    public boolean removeBookItem(BookItem b) { return books.remove(b); }
+
+    public boolean removeRack(Rack r) { return racks.remove(r); }
 }
