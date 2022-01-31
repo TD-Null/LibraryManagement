eb4bc703106d62b31c3eb28828ff925d3198c17a
TD-Null <tdeligero1209@gmail.com>
2022-01-31 15:09:03 -0500

    Adding in exception handler for when the unable to connect to the database.

--- src/main/java/com/example/LibraryManagement/models/io/responses/exceptions/ApiExceptionHandler.java
+++ src/main/java/com/example/LibraryManagement/models/io/responses/exceptions/ApiExceptionHandler.java
@@ -5,20 +5,23 @@ import org.hibernate.JDBCException;
 import org.hibernate.exception.JDBCConnectionException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.CannotCreateTransactionException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.sql.SQLException;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 /*
  * Controller advice used to handle exceptions across the entire
- * application. The Exception Handler used in this class will
- * catch exclusively the ApiRequestException object exceptions.
- * Once an exception has caught, a Response Entity will be returned
- * containing data of the REST API, including its message and time
- * that the exception was thrown, as well as return a given HTTP
- * Status Request code.
+ * application. The Exception Handlers used in this class will
+ * catch exceptions globally during the application's runtime.
+ *
+ * Once an exception has been caught, a Response Entity will be
+ * returned containing data of the REST API, including its message
+ * and time that the exception was thrown, as well as return a given
+ * HTTP Status code.
  */
 @ControllerAdvice
 public class ApiExceptionHandler
@@ -41,7 +44,36 @@ public class ApiExceptionHandler
     {
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
         ApiException apiException = new ApiException(
-                new MessageResponse("Unable to connect to server: " + e.getMessage()),
+                new MessageResponse("Unable to connect to server: " +
+                        "\"" + e.getMessage() + "\"."),
+                status,
+                ZonedDateTime.now(ZoneId.of("Z"))
+        );
+
+        return new ResponseEntity<>(apiException, status);
+    }
+
+    @ExceptionHandler(value = {CannotCreateTransactionException.class})
+    public ResponseEntity<Object> handleCannotCreateTransactionException(CannotCreateTransactionException e)
+    {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        ApiException apiException = new ApiException(
+                new MessageResponse("Unable to create transactions to the server: " +
+                        "\"" + e.getMessage() + "\"."),
+                status,
+                ZonedDateTime.now(ZoneId.of("Z"))
+        );
+
+        return new ResponseEntity<>(apiException, status);
+    }
+
+    @ExceptionHandler(value = {SQLException.class})
+    public ResponseEntity<Object> handleSQLException(SQLException e)
+    {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        ApiException apiException = new ApiException(
+                new MessageResponse("Warnings or errors occurred when accessing the server: " +
+                        "\"" + e.getMessage() + "\"."),
                 status,
                 ZonedDateTime.now(ZoneId.of("Z"))
         );
